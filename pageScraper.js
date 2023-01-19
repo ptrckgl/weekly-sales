4897a61fbe71ecb84bfb51db0e1e29d19285ad65
Patrick <patrickgalea18@gmail.com>
2023-01-19 17:19:13 +1100

    Recursive scrape (using next button) is workinggit add --all

--- pageScraper.js
+++ pageScraper.js
@@ -8,26 +8,98 @@ const scraperObject = {
     console.log(`Navigating to ${this.url}...`);
     await page.goto(this.url);
 
-    textContent = await page.evaluate(() => {
-      return [
-        ...document
-          .querySelector(
-            '#search-content > div > wow-product-search-container > shared-grid > div'
-          )
-          .querySelectorAll('.shelfProductTile-information'),
-      ].map((a) => {
-        return {
-          name: a.querySelector('.shelfProductTile-descriptionLink').innerHTML,
-          price:
-            a.querySelector('.price-dollars').innerHTML +
-            '.' +
-            a.querySelector('.price-cents').innerHTML,
-        };
+    async function scrapeCurrentPage() {
+      console.log(page.url());
+      await waitTillHTMLRendered(page);
+      textContent = await page.evaluate(() => {
+        return [
+          ...document
+            .querySelector(
+              '#search-content > div > wow-product-search-container > shared-grid > div'
+            )
+            .querySelectorAll('.shelfProductTile-information'),
+        ].map((a) => {
+          let obj;
+          try {
+            obj = {
+              name: a.querySelector('.shelfProductTile-descriptionLink')
+                .innerHTML,
+              price:
+                a.querySelector('.price-dollars').innerHTML +
+                '.' +
+                a.querySelector('.price-cents').innerHTML,
+            };
+          } catch (err) {
+            obj = { name: 'ERROR', price: 'ERROR' };
+          }
+          return obj;
+        });
       });
-    });
 
-    console.log(textContent);
+      console.log(textContent);
+
+      let nextButtonExists = false;
+
+      // Checking if the next button exists (ie, if we are on the last page)
+      try {
+        const nextButton = await page.evaluate(() => {
+          return document.querySelector('.paging-next').textContent;
+        });
+        console.log(nextButton);
+        nextButtonExists = true;
+      } catch (err) {
+        nextButtonExists = false;
+      }
+
+      if (nextButtonExists) {
+        console.log('\n\nNEXT BUTTON FOUND AND CLICKED\n\n');
+        await page.click('.paging-next');
+        return scrapeCurrentPage();
+      }
+    }
+
+    await scrapeCurrentPage();
   },
+};
+
+// From https://stackoverflow.com/questions/52497252/puppeteer-wait-until-page-is-completely-loaded
+const waitTillHTMLRendered = async (page, timeout = 30000) => {
+  const checkDurationMsecs = 1000;
+  const maxChecks = timeout / checkDurationMsecs;
+  let lastHTMLSize = 0;
+  let checkCounts = 1;
+  let countStableSizeIterations = 0;
+  const minStableSizeIterations = 3;
+
+  while (checkCounts++ <= maxChecks) {
+    let html = await page.content();
+    let currentHTMLSize = html.length;
+
+    let bodyHTMLSize = await page.evaluate(
+      () => document.body.innerHTML.length
+    );
+
+    console.log(
+      'last: ',
+      lastHTMLSize,
+      ' <> curr: ',
+      currentHTMLSize,
+      ' body html size: ',
+      bodyHTMLSize
+    );
+
+    if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize)
+      countStableSizeIterations++;
+    else countStableSizeIterations = 0; //reset the counter
+
+    if (countStableSizeIterations >= minStableSizeIterations) {
+      console.log('Page rendered fully..');
+      break;
+    }
+
+    lastHTMLSize = currentHTMLSize;
+    await page.waitForTimeout(checkDurationMsecs);
+  }
 };
 
 module.exports = scraperObject;
